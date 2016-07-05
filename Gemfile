1baacd76a9097e3f8f935d4c21594fcaf1d8d21e
Kyo Yamada <hysteric.madam@gmail.com>
2016-07-05 23:22:21 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,45 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.5'
+
+gem 'sqlite3'
+
+gem 'sass-rails', '~> 4.0.2'
+
+gem 'uglifier', '>= 1.3.0'
+
+gem 'coffee-rails', '~> 4.0.0'
+
+gem 'jquery-rails'
+
+gem 'turbolinks'
+
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+    gem 'sdoc', require: false
+end
+
+
+gem "thor"
+gem "unicorn"
+gem "dotenv-rails"
+gem "devise"
+gem "omniauth"
+gem "omniauth-twitter"
+gem "omniauth-facebook"
+gem "slim-rails"
+gem "bourbon"
+gem "neat"
+gem "bitters"
+gem "font-awesome-rails"
+group :development do
+  gem "html2slim"
+end
+
+group :development, :test do
+  gem "pry-rails"
+  gem "pry-doc"
+  gem "pry-remote"
+  gem "pry-stack_explorer"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sewingbooks::Application.config.secret_key_base = '5de3b18886868d222dd2078e39738034a817c5c494fe8343a461384917353c2bb313c33bf9f91b40b292aafed278659477ce9c5f8624f04b2e04a037e96bd7bd'
