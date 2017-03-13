81d231a45acea46c1a7e6f4d40b4403d97298dc8
Kyo Yamada <kyo@Erikas-iMac.local>
2017-03-13 22:26:48 +0900

    [update]skip_invitationの実装
    githubに載ってたコードをそのまま実行したらちゃんと動いた。
    emailやpasswordが設定されてないとsaveできない仕様みたい。
    スキーマでnull => trueとか試してみたけど、あんま関係なさそう。

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -1,19 +1,46 @@
 class LibraryController < ApplicationController
+  before_action :create_type?, only: [:create]
+
   def index
     @book = Book.new
     @user = User.new
   end
 
   def create
-    @book = Book.new(book_params)
-    if @book.save
-      redirect_to action: 'index'
+    case @type
+    when 'book' then
+      result = Book.new(book_params).save
+    when 'user' then
+      result = create_user
+    end
+    
+    if result
+      redirect_to action: 'index', :notice => "#{@type}の追加に成功"
     else
-      render 'index'
+      redirect_to action: 'index', :alert => "#{@type}の追加に失敗"
     end
   end
 
+  def create_user
+    p = params[:user]
+    @user = User.invite!({"email"=>"user@sample.mail", "name"=>p[:name], "dept"=>p[:dept], "role"=>p[:role]}) do |u|
+      u.skip_invitation = true
+    end
+    token = Devise::VERSION >= "3.1.0" ? @user.instance_variable_get(:@raw_invitation_token) : @user.invitation_token
+    User.accept_invitation!(:invitation_token => token, :password => 'sewingbooks_2017', :password_confirmation => 'sewingbooks_2017')
+
+    @user.save
+  end
+
   private
+
+    def create_type?
+      params.each do |key, val|
+        if key == 'book' or key == 'user'
+          @type = key
+        end
+      end
+    end
 
     def book_params
       params.require(:book).permit(
@@ -23,6 +50,14 @@ class LibraryController < ApplicationController
         :recommended_by,
         :comment,
         :recommend_date,
+      )
+    end
+
+    def user_params
+      params.require(:user).permit(
+        :name,
+        :dept,
+        :role,
       )
     end
 end
