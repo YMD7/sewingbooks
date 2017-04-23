fbaa4b7d1f96580b2fd526065e7cd3757d964f81
Kyo Yamada <kyo@Erikas-iMac.local>
2017-04-23 12:24:54 +0900

    [add/update]seeds.rbによる初期データ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# User
+yml = File.read("#{Rails.root}/db/seeds/user.yml")
+list = YAML.load(yml)
+p list
+
+def adduser(attr)
+	@user = User.invite!(attr)
+	token = Devise::VERSION >= "3.1.0" ? @user.instance_variable_get(:@raw_invitation_token) : @user.invitation_token
+  pw    = "sewingbooks_2017"
+	User.accept_invitation!(:invitation_token => token, :password => pw, :password_confirmation => pw)
+	@user
+end
+
+list.each_with_index do |(u),i|
+  attr = {
+    :email => "#{u["user"]}@sample.mail",
+    :name  => u["name"],
+    :dept  => u["dept"],
+    :role  => u["role"],
+    :portrait => open("#{Rails.root}/db/seeds/#{u['portrait']}"),
+    :skip_invitation => true,
+  }
+  p attr[:name]
+  adduser(attr)
+end
+
