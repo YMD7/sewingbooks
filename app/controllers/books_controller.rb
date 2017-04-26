bf978d08965426dd82cbae19b131afe784a159c5
Kyo Yamada <kyo@Erikas-iMac.local>
2017-04-26 22:58:47 +0900

    [update]book showページをモデルを元に実装、図書カード部分だけまだ。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,8 @@
 class BooksController < ApplicationController
+  def show
+    @book  = Book.find(params[:id])
+    @prev_book = Book.find(params[:id].to_i - 1) if Book.exists?(:id => params[:id].to_i - 1)
+    @next_book = Book.find(params[:id].to_i + 1) if Book.exists?(:id => params[:id].to_i + 1)
+    @users = User.all
+  end
 end
